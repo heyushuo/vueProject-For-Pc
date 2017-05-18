10437622974f24717049b3e8a8a37a8dd2f429df
heyushuo <1007561607@qq.com>
2017-05-18 23:17:30 +0800

    继续

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import routerConfig from './Router.config.js'
 Vue.use(VueRouter)
 //配置axios
 axios.defaults.baseURL='http://localhost:8080/';  //设置访问的根路径
-Vue.prototype.$http = axios  //其他页面在使用axios的时候直接  this.$http就可以了
+Vue.prototype.$http = axios; //其他页面在使用axios的时候直接  this.$http就可以了
 const router=new VueRouter({
 	mode: 'history', //切换路径模式，变成history模式
 	scrollBehavior: () => ({ y: 0 }), // 滚动条滚动的行为，不加这个默认就会记忆原来滚动条的位置
